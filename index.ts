423b67aff2399b643d5bd290364fd6ee36694217
imanelmzk <imane.lmzk@exemple.com>
2026-02-20 15:15:33 +0000

    Another LEVEL

--- index.ts
+++ index.ts
@@ -1,4 +1,77 @@
 export{};
+interface Person{
+    name: string;
+    age: number;
+    email: string;
+    isStudent: boolean;
+    hobbies: string[];
+    address: {
+        street: string;
+        city: string;
+        country: string;
+    }
+}
+// Interface OMIT
+interface personWithoutEmailAndHobbies extends Omit<Person, 'email' | 'hobbies' >{}
+// Teste:
+const p:personWithoutEmailAndHobbies={
+    name: "Imane",
+    age: 25,
+    isStudent: true,
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+console.log(p);
+// Interface PICK
+interface personWithAdressAndName extends Pick<Person, 'name' | 'address'> {}
+// Teste:
+const p2: personWithAdressAndName={
+    name: "Imane",
+    address: {
+        street: "123 Main St",  
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+// Interface PARTIAL
+interface optionalPerson extends Partial<Person>{}
+const p3 : optionalPerson ={}
+console.log(p3);
+
+
+
+
+
+/* FONCTIONS GENERIQUES POUR RETOURNER UN ELEMENT D'UN TABLEAU SANS PERDRE LE TYPE
+//🧪 Exemple : fonction générique pour récupérer un élément d’un tableau
+
+//👉 Cas réel : une fonction qui retourne un élément d’un tableau sans perdre le type.
+function getFirstElement <T extends any>(array: T[]): T|undefined {
+    return array[0]; //* || on peut aussi "return array[0]!" pour dire que c'est sûr que le tableau n'est pas vide, mais cela peut causer des erreurs si le tableau est effectivement vide.
+}
+// tests
+//* Numbers
+const numbers = [10, 20, 30];
+const firstNumber = getFirstElement(numbers);
+if(firstNumber !== undefined) {
+    console.log(firstNumber.toFixed(1)); // Affiche: 10.00
+}
+ // Affiche: 10
+
+//* Strings
+const strings = ["hello", "world"];
+const firstString = getFirstElement(strings);
+if (firstString !== undefined) {
+    console.log(firstString.toUpperCase()); // Affiche: "HELLO"
+}
+
+*/
+
+/* Interfaces optionnelles : pour définir des propriétés qui ne sont pas obligatoires dans un objet
 interface Person {
     name: string;
     age?: number;
@@ -9,7 +82,7 @@ let x: Person ={
 }
 console.log(x.name);
 console.log(x.age);
-
+*/
 
 // Autre exemple: quand peut cocher selon les choix proposer 
 // le saison + les jours du semaine....
