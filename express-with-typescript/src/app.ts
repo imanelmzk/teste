ceb4c77d0c464b21a721c4ddb28dd1f27ea60413
imane.lmzk <imane.lmzk@gmail.com>
2026-02-27 00:11:36 +0000

    New use with Framework Express.js

---
+++ express-with-typescript/src/app.ts
@@ -0,0 +1,13 @@
+import express = require("express");
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send(" Hello Express + TypeScript 🚀 ");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
